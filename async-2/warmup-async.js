8cf1a1b7af5e7110daefba4b5f1e089b2e1847b6
Conor McCue <conormccue@gmail.com>
2020-01-24 23:04:05 -0800

    first step

--- async-2/warmup-async.js
+++ async-2/warmup-async.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const superagent = require('superagent');
+const url = 'https://swapi.co/api/people'
+function fetchPeopleWithPromises(){
+  superagent.get(url)
+    .then(res => {
+      return res.body.results.map( person => {
+        person.url;
+      })
+    })
+    .then()
+}
